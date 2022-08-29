07c3c9a52f23f32f3a28bbb3907a216c1bf6ac16
alyamba <logunionok@gmail.com>
2022-08-29 13:03:10 +0300

    fix: add counter-number in deck

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 const ancient = document.querySelectorAll(".ancient");
 const lavel = document.querySelector(".lavel");
-const buttonLavel = document.querySelectorAll(".button-lavel")
+const buttonLavel = document.querySelectorAll(".button-lavel");
 const cardDeckButton = document.querySelector(".card-deck");
 const cardCounter = document.querySelector(".card-counter");
 const deck = document.querySelector(".deck");
 
+const firstGreen = document.querySelector(".first-green");
+const firstBrown = document.querySelector(".first-brown");
+const firstBlue = document.querySelector(".first-blue");
+
+const secondGreen = document.querySelector(".second-green");
+const secondBrown = document.querySelector(".second-brown");
+const secondBlue = document.querySelector(".second-blue");
+
+const thirdGreen = document.querySelector(".third-green");
+const thirdBrown = document.querySelector(".third-brown");
+const thirdBlue = document.querySelector(".third-blue");
 
 let ancientActive;
 
@@ -66,5 +77,58 @@ function deckData() {
   cardDeckButton.classList.add("card-deck-display");
   cardCounter.classList.remove("display");
   deck.classList.remove("display");
+
+  console.log(ancientActive);
+  console.log(buttonActive);
+
+  if (ancientActive === "azathoth") {
+    firstGreen.textContent = 1;
+    firstBrown.textContent = 2;
+    firstBlue.textContent = 1;
+
+    secondGreen.textContent = 2;
+    secondBrown.textContent = 3;
+    secondBlue.textContent = 1;
+
+    thirdGreen.textContent = 2;
+    thirdBrown.textContent = 4;
+    thirdBlue.textContent = 0;
+  } else if (ancientActive === "cthulhu") {
+    firstGreen.textContent = 0;
+    firstBrown.textContent = 2;
+    firstBlue.textContent = 2;
+
+    secondGreen.textContent = 1;
+    secondBrown.textContent = 3;
+    secondBlue.textContent = 0;
+
+    thirdGreen.textContent = 3;
+    thirdBrown.textContent = 4;
+    thirdBlue.textContent = 0;
+  } else if (ancientActive === "iogSothoth") {
+    firstGreen.textContent = 0;
+    firstBrown.textContent = 2;
+    firstBlue.textContent = 1;
+
+    secondGreen.textContent = 2;
+    secondBrown.textContent = 3;
+    secondBlue.textContent = 1;
+
+    thirdGreen.textContent = 3;
+    thirdBrown.textContent = 4;
+    thirdBlue.textContent = 0;
+  } else {
+    firstGreen.textContent = 1;
+    firstBrown.textContent = 2;
+    firstBlue.textContent = 1;
+
+    secondGreen.textContent = 3;
+    secondBrown.textContent = 2;
+    secondBlue.textContent = 1;
+
+    thirdGreen.textContent = 2;
+    thirdBrown.textContent = 4;
+    thirdBlue.textContent = 0;
+  }
 }
-cardDeckButton.addEventListener('click', deckData);
+cardDeckButton.addEventListener("click", deckData);
